ead9225c65fab714cb70a7cbe871371d0ec41527
crnapagoda <30378038+crnapagoda@users.noreply.github.com>
2025-01-26 20:53:16 +0100

    secret file added

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
-SMTP_SERVER = "smtp.office365.com"
-SMTP_PORT = 587
-EMAIL_USERNAME = "" 
-EMAIL_PASSWORD = ""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SMTP_SERVER = os.getenv("SMTP_SERVER")
+SMTP_PORT = int(os.getenv("SMTP_PORT"))
+EMAIL_USERNAME = os.getenv("EMAIL_USERNAME")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 # Twilio konfiguracija
-TWILIO_ACCOUNT_SID = ""  # acc sid
-TWILIO_AUTH_TOKEN = ""  # auth token
-TWILIO_SMS_API_URL = f""
-TWILIO_PHONE_NUMBER = ""  # verifikovan twilio broj telefona +38160123123
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")  # acc sid
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")  # auth token
+TWILIO_SMS_API_URL = os.getenv("TWILIO_SMS_API_URL")  # api url
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")  # verifikovan twilio broj telefona +38160123123
